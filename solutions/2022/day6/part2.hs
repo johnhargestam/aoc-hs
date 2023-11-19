6adbc1643166d9ba9711d59cb8702df3e7c8ba5b
= <john.hargestam@gmail.com>
2023-11-19 21:34:56 +0100

    2022 day 6 part 2

--- solutions/2022/day6/part2.hs
+++ solutions/2022/day6/part2.hs
@@ -1,9 +1,19 @@
 {-# OPTIONS_GHC -Wall #-}
 
 import Day
+import Data.Set (fromList, size)
+
+allUnique :: String -> Bool
+allUnique s = size (fromList s) == length s
+
+fourUniqueIndex :: String -> Int
+fourUniqueIndex = go 14
+  where go i s
+         | allUnique (take 14 s) = i
+         | otherwise             = go (i+1) (drop 1 s)
 
 solution :: String -> String
-solution txt = txt
+solution = show . fourUniqueIndex
 
 main :: IO ()
 main = apply solution

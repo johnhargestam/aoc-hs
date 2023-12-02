ad573d09bee5a6810c8e02fc5a2a4fe8e678be06
= <john.hargestam@gmail.com>
2023-12-02 12:04:34 +0100

    2023 day 2 part 2

--- solutions/2023/day2/Day.hs
+++ solutions/2023/day2/Day.hs
@@ -3,9 +3,37 @@
 module Day where
 
 import Aoc (evaluate)
+import Utils.List (splitEq)
+import Data.Bifunctor (first)
+import Data.List (find)
 
 apply :: (String -> String) -> IO ()
 apply = evaluate "solutions/2023/day2/input"
 
 verify :: (String -> String) -> IO ()
 verify = evaluate "solutions/2023/day2/sample"
+
+data Colors = Colors { red :: Int, green :: Int, blue :: Int } deriving Show
+
+data Game = Game { gameId :: Int, colors :: [Colors] } deriving Show
+
+readColorAmount :: String -> (Int, String)
+readColorAmount = first read . tupled . words
+  where tupled [x,y] = (x, y)
+        tupled _     = undefined
+
+colorAmount :: String -> [(Int, String)] -> Int
+colorAmount color = maybe 0 fst . find isColor
+  where isColor = (==color) . snd
+
+readColors :: String -> Colors
+readColors = toColors . map readColorAmount . splitEq ", "
+  where toColors xs = Colors { red   = colorAmount "red" xs,
+                               green = colorAmount "green" xs,
+                               blue  = colorAmount "blue" xs }
+
+readGame :: String -> Game
+readGame = toGame . splitEq ": "
+  where toGame [x,y] = Game (readId x) (map readColors $ splitEq "; " y)
+        toGame _     = undefined
+        readId = read . last . words
--- solutions/2023/day2/part2.hs
+++ solutions/2023/day2/part2.hs
@@ -2,8 +2,18 @@
 
 import Day
 
+mostColors :: Colors -> Colors -> Colors
+mostColors x y = Colors { red = most red, green = most green, blue = most blue }
+  where most f = max (f x) (f y)
+
+requiredColors :: [Colors] -> Colors
+requiredColors = foldr1 mostColors
+
+power :: Colors -> Int
+power c = red c * green c * blue c
+
 solution :: String -> String
-solution txt = txt
+solution = show . sum . map (power . requiredColors . colors . readGame) . lines
 
 main :: IO ()
 main = apply solution

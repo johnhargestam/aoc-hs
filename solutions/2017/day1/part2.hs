ebb681745d2cdfe088d55a19f76becb4ee076102
= <john.hargestam@gmail.com>
2023-12-02 22:30:39 +0100

    2017 day 1 part 2

--- solutions/2017/day1/part2.hs
+++ solutions/2017/day1/part2.hs
@@ -2,8 +2,17 @@
 
 import Day
 
+match :: (Char, Char) -> Bool
+match (x,y) = x == y
+
+readT :: (Char, Char) -> Int
+readT = read . return . fst
+
+matches :: String -> [Int]
+matches s = map readT $ filter match $ zip s $ drop (length s `div` 2) $ cycle s
+
 solution :: String -> String
-solution txt = txt
+solution = show . sum . matches
 
 main :: IO ()
 main = apply solution

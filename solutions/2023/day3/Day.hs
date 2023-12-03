c33801e747e289e3c930a587347a7e1cbc3eba24
= <john.hargestam@gmail.com>
2023-12-03 23:21:06 +0100

    2023 day 3 part 2

--- solutions/2023/day3/Day.hs
+++ solutions/2023/day3/Day.hs
@@ -3,9 +3,36 @@
 module Day where
 
 import Aoc (evaluate)
+import Data.Char (isDigit)
+import Utils.List (mapWithIndex)
+import Data.List (groupBy)
+import Data.Bifunctor (Bifunctor(first))
 
 apply :: (String -> String) -> IO ()
 apply = evaluate "solutions/2023/day3/input"
 
 verify :: (String -> String) -> IO ()
 verify = evaluate "solutions/2023/day3/sample"
+
+type XY = (Int, Int)
+
+-- ("33",(5,5)) ('4',(7,5)) -> ("334",(5,5))
+joinChars :: (String, XY) -> (Char, XY) -> (String, XY)
+joinChars (s, xy) (c, _) = (s ++ [c], xy)
+
+-- 0 "467..114..*" -> [("467",(0,0)), ("114",(5,0)), ("*",(10,0))]
+line :: Int -> String -> [(String, XY)]
+line y = map toString . filter notDot . groupBy isNumber . mapWithIndex charXY
+  where charXY x c = (c, (x, y))
+        isNumber (a, _) (b, _) = isDigit a && isDigit b
+        notDot = (/='.') . fst . head
+        toString cs = foldl joinChars (first (: []) $ head cs) $ tail cs
+
+tokens :: String -> [(String, XY)]
+tokens = concat . mapWithIndex line . lines
+
+areNeighbours :: (String, XY) -> (String, XY) -> Bool
+areNeighbours (a, (x1, y1)) (b, (x2, y2))
+  | y1 == y2         = x2 - x1 == length a || x1 - x2 == length b
+  | abs (y2-y1) == 1 = x1 + length a >= x2 && x2 + length b >= x1
+  | otherwise        = False
--- solutions/2023/day3/part2.hs
+++ solutions/2023/day3/part2.hs
@@ -1,9 +1,20 @@
 {-# OPTIONS_GHC -Wall #-}
 
 import Day
+import Data.Char (isDigit)
+import Data.List (partition)
+
+toRatio :: [(String, b)] -> Int
+toRatio = product . map (read . fst) 
+
+ratios :: [(String, XY)] -> [Int]
+ratios = map toRatio . filter exactlyTwo . uncurry neighbours . partition isNumber
+  where isNumber = isDigit . head . fst
+        neighbours = map . flip (filter . areNeighbours)
+        exactlyTwo = (==2) . length
 
 solution :: String -> String
-solution txt = txt
+solution = show . sum . ratios . tokens
 
 main :: IO ()
 main = apply solution

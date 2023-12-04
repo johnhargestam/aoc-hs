71496bcaa0862a35bac8ff44b24edd0fc25c7491
= <john.hargestam@gmail.com>
2023-12-04 09:18:16 +0100

    2023 day 4 part 1

---
+++ solutions/2023/day4/Day.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Day where
+
+import Aoc (evaluate)
+
+apply :: (String -> String) -> IO ()
+apply = evaluate "solutions/2023/day4/input"
+
+verify :: (String -> String) -> IO ()
+verify = evaluate "solutions/2023/day4/sample"
